763ea54915edcb76f18fa4e74b96eeb0a9214dda
Sven Schrodt <sven@schrodt.club>
2025-03-06 16:30:18 +0100

    Adding sepia()

--- intro.py
+++ intro.py
@@ -4,11 +4,11 @@ from PIL import Image
 import numpy as np
 
 
-class PILFilter:
+class ImgFilter:
     """" Applying different filters to PIL.* instances  """
     
     def change_value_by(self, img: Image.Image, value: int)->Image.Image:
-        """ Changing each value by given value (+/-) """
+        """ Changing each value by given value (+/-) - generic function """
         # Converting PII to np.array
         tmp_arr = np.array(img)
         # Setting (+/-) value
@@ -16,11 +16,31 @@ class PILFilter:
         # Limiting each value to one byte's range
         np.clip(tmp_arr, 0, 255)
         
-        # returning np array converted back to P.I.I
+        # Returning np array converted back to P.I.I
         return Image.fromarray(tmp_arr)
+    
+    def sepia(img: Image.Image, intensity: int = 0.6)->Image.Image:
+        # SEE: https://drafts.fxtf.org/filter-effects/#sepiaEquivalent 
+            tmp = np.array(img)
+
+            # Tranforming each pixel's r,g,b parts
+
+            lmap = np.matrix([
+                                [ 0.393 + 0.607 * (1 - intensity), 0.769 - 0.769 * (1 - intensity), 0.189 - 0.189 * (1 - intensity)],
+                                [ 0.349 - 0.349 * (1 - intensity), 0.686 + 0.314 * (1 - intensity), 0.168 - 0.168 * (1 - intensity)],
+                                [ 0.272 - 0.349 * (1 - intensity), 0.534 - 0.534* (1 - intensity), 0.131 + 0.869 * (1 - intensity)]
+            ])
+
+            filter_mapper = np.array([x * lmap.T for x in img] )
+
+            # Sanitizing values
+            np.clip(filter_mapper, 0, 255)
+
+            # Returning np array converted back to P.I.I
+            return Image.fromarray(filter_mapper.astype('uint8'))
 
 
-filter = PILFilter()
+filter = ImgFilter()
 first = Image.open('ernstes_bild.jpg')
 dim = first.size
 print(dim)
